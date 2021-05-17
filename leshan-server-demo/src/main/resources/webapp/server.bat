a0e7a6f806ec7d5e3fdbfbb5a93b71f3e4e083ba
Benoit Leforestier <benoit.leforestier@sheeld.co>
2021-05-17 14:29:34 +0200

    Move http launcher

--- leshan-server-demo/src/main/resources/webapp/server.bat
+++
@@ -1,2 +0,0 @@
-rem https://github.com/http-party/http-server
-http-server --port 8081 --proxy http://localhost:8080 --cors -c-1
---
+++ leshan-server-demo/start_http.bat
@@ -0,0 +1,2 @@
+rem https://github.com/http-party/http-server
+http-server src\main\resources\webapp --port 8081 --proxy http://localhost:8080 --cors -c-1
