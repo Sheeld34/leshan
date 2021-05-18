4e0037d98e625536d43be338b89859340057a6d2
Benoit Leforestier <benoit.leforestier@sheeld.co>
2021-05-18 13:19:28 +0200

    Use script path as working path

--- leshan-server-demo/start_http.bat
+++ leshan-server-demo/start_http.bat
@@ -1,2 +1,6 @@
+@echo off
 rem https://github.com/http-party/http-server
-http-server src\main\resources\webapp --port 8081 --proxy http://localhost:8080 --cors -c-1
+set scriptPath=%~dp0
+set proxyUrl=http://localhost:8080
+
+http-server %scriptPath%src\main\resources\webapp --port 8081 --proxy %proxyUrl% --cors -c-1
