12db90f839c686c804360b71eab17665be83b254
Benoit Leforestier <benoit.leforestier@sheeld.co>
2021-05-19 15:43:01 +0200

    Revert "Add installation instruction for letsencrypt/certbot"
    This reverts commit 1ad40bfc2538fed298b284084fccb90288b0901e.

--- leshan-server-demo/start_http.bat
+++ leshan-server-demo/start_http.bat
@@ -1,6 +1,6 @@
 @echo off
 rem https://github.com/http-party/http-server
 set scriptPath=%~dp0
-set proxyUrl=http://demo.iot.sheeld.co:8080
+set proxyUrl=http://localhost:8080
 
 http-server %scriptPath%src\main\resources\webapp --port 8081 --proxy %proxyUrl% --cors -c-1
