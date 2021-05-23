a128a953c5e845d2badb07c4a089d29207d706a2
Benoit Leforestier <benoit.leforestier@sheeld.co>
2021-05-23 23:01:10 +0200

    Add parameter

--- leshan-server-demo/start_http.bat
+++ leshan-server-demo/start_http.bat
@@ -1,6 +1,7 @@
 @echo off
 rem https://github.com/http-party/http-server
 set scriptPath=%~dp0
-set proxyUrl=http://localhost:8080
+
+if "%~1"=="" (set proxyUrl=http://localhost:8080) else (set proxyUrl=%1)
 
 http-server %scriptPath%src\main\resources\webapp --port 8081 --proxy %proxyUrl% --cors -c-1
